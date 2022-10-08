1cfcd9c45345165bb7fb4c7669edb94a49d5b681
Masa-Ryu <privatead.ryu@gmail.com>
2022-10-08 19:51:01 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='twpy',
+        version='1.0',
+        description='Twitter wrapper for V1 and V2 API',
+        author='Masa-Ryu',
+        author_email='',
+        url='https://github.com/Masa-Ryu/tcpy',
+        packages=[],
+        )
---
+++ tests/test_wrapper.py
@@ -0,0 +1,22 @@
+from base64 import b64encode
+import twpy.wrapper
+
+from rich import print
+
+if __name__ == '__main__':
+    wrapper = twpy.wrapper.Wrapper()
+    # resp = wrapper.rate_limit_status()
+    # resp = wrapper.update(f'Web3イベントは可愛い子が多いので敷居が高くなっていると思います。活動頑張ってください！')
+    # resp = wrapper.destroy(resp['id'])
+    # resp = wrapper.media(open('nikeeta_note.jpg', 'rb'))
+    # resp = wrapper.verify_credentials()
+    # resp = wrapper.tweet_lookup()
+    # resp = wrapper.user_tweet_timeline_by_id('1142625173524238336')
+    # resp = wrapper.user_by_id(['1142625173524238336', '1394158690341556224'])
+    # resp = wrapper.recent_research('ビットコイン')
+    # resp = wrapper.following('1142625173524238336')
+    with open('blank.png', "rb") as image_file:
+        data = b64encode(image_file.read())
+    resp = wrapper.update_profile_image(data)
+    print(resp)
+
